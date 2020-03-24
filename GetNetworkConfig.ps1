5a96fb05f314c35b0e0318c5ba6ed0634c3a10e9
Golan Cohen <golan13@gmail.com>
2020-03-24 15:40:43 +0200

    added asa and fortigate compatibility

--- GetNetworkConfig.ps1
+++ GetNetworkConfig.ps1
@@ -14,33 +14,18 @@ function Get-DeviceConfig
     [OutputType([String])]
     param
     (
-		[Parameter(Mandatory=$true)]
 		[String]$HostAddress,
-		[Parameter(Mandatory=$false)]
-		[Int]$HostPort = 22,
-        [Parameter(Mandatory=$true)]
+		[Int]$HostPort,
         [String]$Vendor,
-		[Parameter(Mandatory=$true)]
 		[String]$Username,
-        [Parameter(Mandatory=$True)]
-        [String]$Password,
-		[Parameter(Mandatory=$false)]
-		[Switch]$AcceptKey,
-		[Parameter(Mandatory=$true)]
+        [SecureString]$Password,
 		[String]$Command,
-        [Parameter(Mandatory=$false)]
         [String]$Output,
-        [Parameter(Mandatory=$false)]
         [Switch]$a,
-        [Parameter(Mandatory=$false)]
-        [Int] $Timeout = 5
+        [Int]$Timeout
     )
-    if ([string]::IsNullOrEmpty($HostPort)) {
-        $HostPort = 22;
-    }
-    $SecPassword = ConvertTo-SecureString -String $Password -AsPlainText -Force;
-    $Credentials = New-Object System.Management.Automation.PSCredential $Username, $SecPassword;
-    $SSHSession = New-SSHSession -ComputerName $HostAddress -Port $HostPort -Credential $Credentials -AcceptKey:$AcceptKey;   
+    $Credentials = New-Object System.Management.Automation.PSCredential $Username, $Password;
+    $SSHSession = New-SSHSession -ComputerName $HostAddress -Port $HostPort -Credential $Credentials -AcceptKey ;   
     if ($SSHSession.Connected)
     {
         $SessionStream = New-SSHShellStream -SessionId $SSHSession.SessionId;
@@ -57,6 +42,13 @@ function Get-DeviceConfig
             $SessionStream.WriteLine("set cli screen-width 1000");
         } elseif ($Vendor -eq "enterasys"){
             $SessionStream.WriteLine("terminal more disable");
+        } elseif ($Vendor -eq "fortigate") {
+            $SessionStream.WriteLine("config system console");
+            $SessionStream.WriteLine("set output standard");
+        } elseif ($Vendor -eq "asa") {
+            $SessionStream.WriteLine("enable");
+            $SessionStream.WriteLine("terminal pager 0");
+            $SessionStream.WriteLine("no pager");
         }
         $SessionStream.WriteLine($command);
         Start-Sleep -s $Timeout;
@@ -110,7 +102,7 @@ function Check-Table
                 Write-Host Found empty cell '('$i','$j ')' -ForegroundColor Red;
                 $fix = $true;
             } elseif ($j -eq 5) {
-                if ($cell -ne "cisco" -and $cell -ne "hp" -and $cell -ne "h3c" -and $cell -ne "juniper" -and $cell -ne "enterasys") {
+                if ($cell -ne "cisco" -and $cell -ne "hp" -and $cell -ne "h3c" -and $cell -ne "juniper" -and $cell -ne "enterasys" -and $cell -ne "fortigate" -and $cell -ne "asa") {
                     Write-Host Unknown vendor on row $i : "'"${vendor} "'" -ForegroundColor Red
                     $fix = $true 
                 }
@@ -118,14 +110,14 @@ function Check-Table
         }
     }
     if ($fix){
-        Write-Host "`nVendor must be: cisco ios, cisco nexus, hp, h3c, terrasys or juniper"
+        Write-Host "`nVendor must be: cisco, asa, hp, h3c, fortiage, enterasys or juniper"
         Write-Host “`nPlease fix excel and re-run the script”
         Read-Host "`nPress ENTER to exit"
         $wb.close($false)
         $excel.Quit()
         exit
     } else {
-        Write-Host "`nFormat good" -ForegroundColor Green
+        Write-Host "`nFormat Good" -ForegroundColor Green
     }
 }
 
@@ -149,6 +141,7 @@ function Rename-Dir
 Read-Host “Please choose location of excel file (Press ENTER)”
 #$loc = Get-FileName
 $loc = "C:\Users\Golan\Documents\NetConfigCollector\test.xlsx"
+if (!($Timeout = Read-Host "Timeout in seconds between each run [default = 5]")) { $Timeout = 5 };
 $IPCol = 1
 $PortCol = 2
 $UserCol = 3
@@ -161,11 +154,14 @@ $sh = $wb.Sheets.Item(1)
 $length = $sh.UsedRange.Rows.Count;
 Check-Table;
 Write-Host "`nCreating connection and retrieving configuration files. Please wait.`n"
-$dir = [string](Split-Path -Path $loc) + '\Config'
+$dir = [String](Split-Path -Path $loc) + '\Config'
 mkdir $dir | Out-Null
 for ($i = $StartRow; $i -le $length; $i++){
     $ip = $sh.Cells.Item($i, $IPCol).Text
-    $port = $sh.Cells.Item($i, $PortCol).Text
+    $port = $null ; $port = $sh.Cells.Item($i, $PortCol).Text
+    if ([String]::IsNullOrEmpty($port)) {
+        $port = 22;
+    }
     $username = $sh.Cells.Item($i, $UserCol).Text
     $password = $sh.Cells.Item($i, $PassCol).Text
     $vendor = $sh.Cells.Item($i, $VendorCol).Text
@@ -173,36 +169,42 @@ for ($i = $StartRow; $i -le $length; $i++){
     switch($vendor)
     {
         "cisco"{
-            Get-DeviceConfig -HostAddress $ip -Username $username -Password $password -AcceptKey -Vendor "cisco" -Command "sh run" -Output $dir\$i\'sh run.txt'
-            Get-DeviceConfig -HostAddress $ip -Username $username -Password $password -AcceptKey -Vendor "cisco" -Command "show ip route vrf *" -Output $dir\$i\'route.txt'
-            Get-DeviceConfig -HostAddress $ip -Username $username -Password $password -AcceptKey -Vendor "cisco" -Command "sh snmp user" -Output $dir\$i\'snmp.txt'
-            Get-DeviceConfig -HostAddress $ip -Username $username -Password $password -AcceptKey -Vendor "cisco" -Command "sh conf | include hostname" -Output $dir\$i\'run.txt'
-            Get-DeviceConfig -HostAddress $ip -Username $username -Password $password -AcceptKey -Vendor "cisco" -Command "sh ver" -Output $dir\$i\'run.txt' -a
-            Get-DeviceConfig -HostAddress $ip -Username $username -Password $password -AcceptKey -Vendor "cisco" -Command "show access-lists" -Output $dir\$i\'run.txt' -a
-
+            Get-DeviceConfig -HostAddress $ip -HostPort $port -Username $username -Password $password -Vendor "cisco" -Command "sh run" -Output $dir\$i\'sh run.txt'
+            Get-DeviceConfig -HostAddress $ip -HostPort $port -Username $username -Password $password -Vendor "cisco" -Command "show ip route vrf *" -Output $dir\$i\'route.txt'
+            Get-DeviceConfig -HostAddress $ip -HostPort $port -Username $username -Password $password -Vendor "cisco" -Command "sh conf | include hostname" -Output $dir\$i\'run.txt'
+            Get-DeviceConfig -HostAddress $ip -HostPort $port -Username $username -Password $password -Vendor "cisco" -Command "sh ver" -Output $dir\$i\'run.txt' -a
+            Get-DeviceConfig -HostAddress $ip -HostPort $port -Username $username -Password $password -Vendor "cisco" -Command "show access-lists" -Output $dir\$i\'run.txt' -a
         }
         "h3c"{
-             Get-DeviceConfig -HostAddress $ip -Username $username -Password $password -AcceptKey -Vendor "h3c" -Command "display" -Output $dir\$i\'run.txt'
-             Get-DeviceConfig -HostAddress $ip -Username $username -Password $password -AcceptKey -Vendor "h3c" -Command "display ip routing-table" -Output $dir\$i\'route.txt'
+             Get-DeviceConfig -HostAddress $ip -HostPort $port -Username $username -Password $password -Vendor "h3c" -Command "display" -Output $dir\$i\'run.txt'
+             Get-DeviceConfig -HostAddress $ip -HostPort $port -Username $username -Password $password -Vendor "h3c" -Command "display ip routing-table" -Output $dir\$i\'route.txt'
         }
         "hp"{
-            Get-DeviceConfig -HostAddress $ip -Username $username -Password $password -AcceptKey -Vendor "hp" -Command "sh run" -Output $dir\$i\'run.txt'
-            Get-DeviceConfig -HostAddress $ip -Username $username -Password $password -AcceptKey -Vendor "hp" -Command "show ip route" -Output $dir\$i\'route.txt'
+            Get-DeviceConfig -HostAddress $ip -HostPort $port -Username $username -Password $password -Vendor "hp" -Command "sh run" -Output $dir\$i\'run.txt'
+            Get-DeviceConfig -HostAddress $ip -HostPort $port -Username $username -Password $password -Vendor "hp" -Command "show ip route" -Output $dir\$i\'route.txt'
         }
         "juniper"{
-            Get-DeviceConfig -HostAddress $ip -Username $username -Password $password -AcceptKey -Vendor "juniper" -Command "show configuration | display inheritance | no-more" -Output $dir\$i\'run.txt'
-            Get-DeviceConfig -HostAddress $ip -Username $username -Password $password -AcceptKey -Vendor "juniper" -Command "show chassis hardware | no-more" -Output $dir\$i\'run.txt' -a
-            Get-DeviceConfig -HostAddress $ip -Username $username -Password $password -AcceptKey -Vendor "juniper" -Command "show route logical-system all | no-more" -Output $dir\$i\'route.txt'
-            Get-DeviceConfig -HostAddress $ip -Username $username -Password $password -AcceptKey -Vendor "juniper" -Command "show route all | no-more" -Output $dir\$i\'route1.txt'
+            Get-DeviceConfig -HostAddress $ip -HostPort $port -Username $username -Password $password -Vendor "juniper" -Command "show configuration | display inheritance | no-more" -Output $dir\$i\'run.txt'
+            Get-DeviceConfig -HostAddress $ip -HostPort $port -Username $username -Password $password -Vendor "juniper" -Command "show chassis hardware | no-more" -Output $dir\$i\'run.txt' -a
+            Get-DeviceConfig -HostAddress $ip -HostPort $port -Username $username -Password $password -Vendor "juniper" -Command "show route logical-system all | no-more" -Output $dir\$i\'route.txt'
+            Get-DeviceConfig -HostAddress $ip -HostPort $port -Username $username -Password $password -Vendor "juniper" -Command "show route all | no-more" -Output $dir\$i\'route1.txt'
         }
         "enterasys"{
-            Get-DeviceConfig -HostAddress $ip -Username $username -Password $password -AcceptKey -Vendor "enterasys" -Command "show config all" -Output $dir\$i\'run.txt'
-            Get-DeviceConfig -HostAddress $ip -Username $username -Password $password -AcceptKey -Vendor "enterasys" -Command "show ip route" -Output $dir\$i\'route.txt'
+            Get-DeviceConfig -HostAddress $ip -HostPort $port -Username $username -Password $password -Vendor "enterasys" -Command "show config all" -Output $dir\$i\'run.txt'
+            Get-DeviceConfig -HostAddress $ip -HostPort $port -Username $username -Password $password -Vendor "enterasys" -Command "show ip route" -Output $dir\$i\'route.txt'
         }
-        #add fortigate, cisco asa
+        "fortigate"{
+            Get-DeviceConfig -HostAddress $ip -HostPort $port -Username $username -Password $password -Vendor "fortigate" -Command "get system status" -Output $dir\$i\'config.txt'
+            Get-DeviceConfig -HostAddress $ip -HostPort $port -Username $username -Password $password -Vendor "fortigate" -Command "show" -Output $dir\$i\'config.txt' -a
+            Get-DeviceConfig -HostAddress $ip -HostPort $port -Username $username -Password $password -Vendor "fortigate" -Command "get router info routing-table" -Output $dir\$i\'route.txt'
+        }
+        "asa"{
+            Get-DeviceConfig -HostAddress $ip -HostPort $port -Username $username -Password $password -Vendor "asa" -Command "show run" -Output $dir\$i\'run.txt'
+            Get-DeviceConfig -HostAddress $ip -HostPort $port -Username $username -Password $password -Vendor "asa" -Command "show access-lists" -Output $dir\$i\'run.txt' -a
+            Get-DeviceConfig -HostAddress $ip -HostPort $port -Username $username -Password $password -Vendor "asa" -Command "show route" -Output $dir\$i\'route.txt'
+        }
     }
     Rename-Dir -Num $i -Dir $dir -IP $ip -File $dir\$i\'run.txt'
 }
-
 $wb.close($false)
 $excel.Quit()
